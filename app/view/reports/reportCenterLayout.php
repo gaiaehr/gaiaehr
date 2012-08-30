bf09363c8ce8a3d4a8459c92bf95ae32901ec449
Gino Rivera Falu <hrivera@gi-technologies.com>
2012-08-30 17:49:36 -0400

    Report Center : Changes.

--- app/view/reports/reportCenterLayout.php
+++ app/view/reports/reportCenterLayout.php
@@ -3,33 +3,12 @@
 include_once('langs/en_US.php');
 ?>
 <html>
+<head>
+</head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <link rel="stylesheet" type="text/css" href="app/view/reports/css/reportCenter.css" >
 <body>
-	<script type="text/javascript">
-	function loadjscssfile(filename, filetype)
-	{
-		if (filetype=="js")
-		{ //if filename is a external JavaScript file
-	  		var fileref=document.createElement('script')
-	  		fileref.setAttribute("type","text/javascript")
-	  		fileref.setAttribute("src", filename)
-	 	}
-	 	else if (filetype=="css")
-	 	{ //if filename is an external CSS file
-	  		var fileref=document.createElement("link")
-	  		fileref.setAttribute("rel", "stylesheet")
-	  		fileref.setAttribute("type", "text/css")
-	  		fileref.setAttribute("href", filename)
-	 	}
-	 	if (typeof fileref!="undefined") document.getElementsByTagName("head")[0].appendChild(fileref)
-	}
-	</script>
-	
-	<div class="headerReportCenter">
-		<h2>Gaia EHR - <?=$LANG['report_center']?></h2>
-	</div>
-	
+	<script type="text/javascript" src="app/view/reports/loadJS.js"></script>
 	<div class="CategoryContainer">
 		<span class="title"><?=$LANG['patient_reports']?></span>
 		<ul>

78e527c7ea2e050fa8db318ab1c829f2d5f71feb
Gino Rivera Falu <hrivera@gi-technologies.com>
2012-09-19 14:48:54 -0400

    Reports: New directory host for reports layouts.

--- app/view/reports/ClientListReport.js
+++ app/view/reports/ClientListReport.js
@@ -105,7 +105,7 @@ Ext.define('App.view.reports.ClientListReport', {
 							endDate: me.FilterForm.getForm().findField("to").getValue(),
 							pdf: false
 						};
-						Ext.get('pdfRender').dom.src = 'app/view/reports/templates/ClientListReport.rpt.php?params=' + Ext.JSON.encode(jsonPayload);
+						Ext.get('pdfRender').dom.src = 'modules/reports/ClientListReport.rpt.php?params=' + Ext.JSON.encode(jsonPayload);
 					}
 				},
 				'-',

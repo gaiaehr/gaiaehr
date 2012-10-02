e3b57412309c8ab1d5226b0822779f37b2dac8da
Gino Rivera Falu <hrivera@gi-technologies.com>
2012-10-01 21:21:14 -0400

    New libraries: -PHPWord -PHPExcel -PHPPowerPoint
    This to improve the document exchange between the user and GaiaEHR
    
    Library Removed:
    - TCP PDF

---
+++ lib/PHPPowerPoint/Tests/03serialized.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * PHPPowerPoint
+ *
+ * Copyright (c) 2009 - 2010 PHPPowerPoint
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category   PHPPowerPoint
+ * @package    PHPPowerPoint
+ * @copyright  Copyright (c) 2009 - 2010 PHPPowerPoint (http://www.codeplex.com/PHPPowerPoint)
+ * @license    http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt	LGPL
+ * @version    0.1.0, 2009-04-27
+ */
+
+/** Error reporting */
+error_reporting(E_ALL);
+
+/** Include path **/
+set_include_path(get_include_path() . PATH_SEPARATOR . '../Classes/');
+
+/** PHPPowerPoint */
+include 'PHPPowerPoint.php';
+
+/** PHPPowerPoint_IOFactory */
+include 'PHPPowerPoint/IOFactory.php';
+
+// Create new PHPPowerPoint object
+echo date('H:i:s') . " Create new PHPPowerPoint object\n";
+$objPHPPowerPoint = new PHPPowerPoint();
+
+// Set properties
+echo date('H:i:s') . " Set properties\n";
+$objPHPPowerPoint->getProperties()->setCreator("Maarten Balliauw");
+$objPHPPowerPoint->getProperties()->setLastModifiedBy("Maarten Balliauw");
+$objPHPPowerPoint->getProperties()->setTitle("Office 2007 PPTX Test Document");
+$objPHPPowerPoint->getProperties()->setSubject("Office 2007 PPTX Test Document");
+$objPHPPowerPoint->getProperties()->setDescription("Test document for Office 2007 PPTX, generated using PHP classes.");
+$objPHPPowerPoint->getProperties()->setKeywords("office 2007 openxml php");
+$objPHPPowerPoint->getProperties()->setCategory("Test result file");
+
+// Create slide
+echo date('H:i:s') . " Create slide\n";
+$currentSlide = $objPHPPowerPoint->getActiveSlide();
+
+// Create a shape (drawing)
+echo date('H:i:s') . " Create a shape (drawing)\n";
+$shape = $currentSlide->createDrawingShape();
+$shape->setName('PHPPowerPoint logo');
+$shape->setDescription('PHPPowerPoint logo');
+$shape->setPath('./images/phppowerpoint_logo.gif');
+$shape->setHeight(36);
+$shape->setOffsetX(10);
+$shape->setOffsetY(10);
+//$shape->setRotation(25);
+$shape->getShadow()->setVisible(true);
+$shape->getShadow()->setDirection(45);
+$shape->getShadow()->setDistance(10);
+
+// Create a shape (text)
+echo date('H:i:s') . " Create a shape (rich text)\n";
+$shape = $currentSlide->createRichTextShape();
+$shape->setHeight(300);
+$shape->setWidth(600);
+$shape->setOffsetX(170);
+$shape->setOffsetY(180);
+$shape->getAlignment()->setHorizontal( PHPPowerPoint_Style_Alignment::HORIZONTAL_CENTER );
+$textRun = $shape->createTextRun('Thank you for using PHPPowerPoint!');
+$textRun->getFont()->setBold(true);
+$textRun->getFont()->setSize(60);
+$textRun->getFont()->setColor( new PHPPowerPoint_Style_Color( 'FFC00000' ) );
+
+// Save serialized file
+echo date('H:i:s') . " Write to serialized format\n";
+$objWriter = PHPPowerPoint_IOFactory::createWriter($objPHPPowerPoint, 'Serialized');
+$objWriter->save(str_replace('.php', '.phppt', __FILE__));
+
+// Read from serialized file
+echo date('H:i:s') . " Read from serialized format\n";
+$objPHPPowerPoint = PHPPowerPoint_IOFactory::load(str_replace('.php', '.phppt', __FILE__));
+
+// Save PowerPoint 2007 file
+echo date('H:i:s') . " Write to PowerPoint2007 format\n";
+$objWriter = PHPPowerPoint_IOFactory::createWriter($objPHPPowerPoint, 'PowerPoint2007');
+$objWriter->save(str_replace('.php', '.pptx', __FILE__));
+
+// Echo memory peak usage
+echo date('H:i:s') . " Peak memory usage: " . (memory_get_peak_usage(true) / 1024 / 1024) . " MB\r\n";
+
+// Echo done
+echo date('H:i:s') . " Done writing file.\r\n";

accfcbec1418e90f18bb1560bd62cee5d52373ba
Gino Rivera Falu <hrivera@gi-technologies.com>
2012-10-11 12:37:22 -0400

    Added: X12 835 Dispenser class library.

---
+++ classes/X12_835_Dispenser.class.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * X12 835 Dispenser Class
+ * version: 0.0.1
+ * Desc: This class library will generate a ANSI X12 835 version 5010
+ * from the GaiaEHR database
+ *  
+ */
+
+class Dispenser835()
+{
+	
+	function __construct()
+	{
+		
+	}
+	
+	/*
+	 * Will gather all the data and create a well formatted X12 835 5010
+	 */
+	function Generate835()
+	{
+		
+	}
+}
+
+?>

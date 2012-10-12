5e1dcd7ec17c5812b9316079c7d6934de279385a
Gino Rivera Falu <hrivera@gi-technologies.com>
2012-10-12 14:08:02 -0400

    X12 Class progress...

--- classes/X12_5010.class.php
+++ classes/X12_5010.class.php
@@ -7,22 +7,67 @@
  * from the GaiaEHR database
  *  
  */
+ 
+/*
+ * Invoke the session
+ * This is important.
+ */
+if(!isset($_SESSION)) 
+{
+	session_name('GaiaEHR');
+	session_start();
+	session_cache_limiter('private');
+}
 
-class Dispenser835
+/*
+ * Reset the counter
+ */
+$_SESSION['site']['flops'] = 0;
+
+/*
+ * Invoke the Database Class Library
+ */
+include_once($_SESSION['site']['root'].'/classes/dbHelper.php');
+
+class x12_5010
 {
 	
+	private $dataBase;
+	
 	function __construct()
+	{
+	    $this->$dataBase = new dbHelper();
+	    return;
+	}
+	
+	/*
+	 * Will gather all the data and create a well formatted X12 835 5010
+	 * -- Health Care Claim Payment/Advice (The halth insurance send the answer to the clinic of doctor)
+	 */
+	public static function Generate835()
 	{
 		
 	}
 	
 	/*
-	 * Will gather all the data and create a well formatted X12 835 5010
+	 * Parse and check if the file is correctly formated X12 835 5010 Format
+	 * -- Health Care Claim Payment/Advice (The halth insurance send the answer to the clinic of doctor)
 	 */
-	function Generate835()
+	public static function Parse835()
 	{
 		
 	}
+	
+	/*
+	 * Parse and check if the file is correctly formatted x12 837 5010 Format
+	 * --  	Health Care Claim (The the clinic or doctor sending the claim to the insurance)
+	 */
+	public static function Parse837()
+	{
+		
+	}
+	
 }
+
 
 ?>

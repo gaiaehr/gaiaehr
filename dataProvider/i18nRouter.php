3c0c4d73bb71412e72d9c3e130e6010cd228ef6a
Ernesto J Rodriguez <vela1606@gmail.com>
2012-10-18 03:50:33 -0400

    GAIAEH-127 default language error during installation

--- dataProvider/i18nRouter.php
+++ dataProvider/i18nRouter.php
@@ -25,16 +25,26 @@ class i18nRouter
 	// -----------------------------------------------------------------------
 	public static function getTranslation()
 	{
+		if(isset($_SESSION['site']['localization'])){
+			$loc = $_SESSION['site']['localization'];
+		}elseif(isset($_SESSION['site']['default_localization'])){
+			$loc = $_SESSION['site']['default_localization'];
+		}else{
+			$loc = false;
+		}
 		// This language file is need by default.
 		include($_SESSION['root'] . '/langs/en_US.php');
 		$en_US = $LANG;
-		
-		// This file will be called when the user or the administrator select 
+		// This file will be called when the user or the administrator select
 		// a diferent language. But the primary language will be called first.
 		// So if some words are not translated by the selected language it can be 
-		// displayed by the original language. 
-		include($_SESSION['root'] . '/langs/' . $_SESSION['site']['localization'] . '.php');
-		return array_merge($en_US, $LANG);
+		// displayed by the original language.
+		if($loc !== false){
+			include($_SESSION['root'] . '/langs/' . $loc . '.php');
+			return array_merge($en_US, $LANG);
+		}else{
+			return $en_US;
+		}
 	}
 
 	// -----------------------------------------------------------------------

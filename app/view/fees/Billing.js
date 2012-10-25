3938e268b74577f3e92756de90cf7b5d82b53387
Gino Rivera Falu <hrivera@gi-technologies.com>
2012-10-24 20:30:51 -0400

    GPL Comments.

--- app/view/fees/Billing.js
+++ app/view/fees/Billing.js
@@ -1,11 +1,22 @@
-//******************************************************************************
-// Billing.ejs.php
-// Billing Forms
-// v0.0.1
-// Author: Emmanuel J. Carrasquillo
-// Modified:
-// GaiaEHR (Electronic Health Records) 2011
-//******************************************************************************
+/*
+	GaiaEHR (Electronic Health Records)
+	Billing.ejs.php
+	Billing Forms
+    Copyright (C) 2012 Emmanuel J. Carrasquillo
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 Ext.define('App.view.fees.Billing',
 {
 	extend : 'App.classes.RenderPanel',

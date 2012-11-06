e5f87909b0202b36075aa680e8bb27dfc1244178
Gino Rivera Falu <hrivera@gi-technologies.com>
2012-11-06 10:54:41 -0400

    More on GPL comments.

--- app/model/fees/Billing.js
+++ app/model/fees/Billing.js
@@ -1,36 +1,86 @@
-/**
- * Created by JetBrains PhpStorm.
- * User: Ernesto J. Rodriguez (Certun)
- * File:
- * Date: 2/18/12
- * Time: 11:09 PM
+/*
+ GaiaEHR (Electronic Health Records)
+ Billing.js
+ Billing Store
+ Copyright (C) 2012 Ernesto J. Rodriguez
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+Ext.Ext.define('App.model.fees.Billing',
+{
+	extend : 'Ext.data.Model',
+	fields : [
+	{
+		name : 'eid',
+		type : 'int '
+	},
+	{
+		name : 'pid',
+		type : 'int'
+	},
+	{
+		name : 'patientName',
+		type : 'string'
+	},
+	{
+		name : 'primaryProvider',
+		type : 'string'
+	},
+	{
+		name : 'encounterProvider',
+		type : 'string'
+	},
+	{
+		name : 'supervisorProvider',
+		type : 'string'
+	},
+	{
+		name : 'facility',
+		type : 'string'
+	},
+	{
+		name : 'billing_facility',
+		type : 'string'
+	},
+	{
+		name : 'service_date',
+		type : 'string'
+	},
+	{
+		name : 'close_date',
+		type : 'string'
+	},
+	{
+		name : 'billing_stage',
+		type : 'int'
+	},
+	{
+		name : 'icdxCodes',
+		type : 'auto'
+	}],
+	proxy :
+	{
+		type : 'direct',
+		api :
+		{
+			read : Fees.getFilterEncountersBillingData
+		},
+		reader :
+		{
+			root : 'encounters',
+			totalProperty : 'totals'
+		}
+	}
 
-Ext.define('App.model.fees.Billing', {
-    extend: 'Ext.data.Model',
-    fields: [
-        {name: 'eid', type: 'int '},
-        {name: 'pid', type: 'int'},
-        {name: 'patientName', type: 'string'},
-        {name: 'primaryProvider', type: 'string'},
-        {name: 'encounterProvider', type: 'string'},
-        {name: 'supervisorProvider', type: 'string'},
-        {name: 'facility', type: 'string'},
-        {name: 'billing_facility', type: 'string'},
-        {name: 'service_date', type: 'string'},
-        {name: 'close_date', type: 'string'},
-        {name: 'billing_stage', type: 'int'},
-        {name: 'icdxCodes', type: 'auto'}
-    ],
-    proxy : {
-        type: 'direct',
-        api : {
-            read  : Fees.getFilterEncountersBillingData
-        },
-        reader     : {
-            root: 'encounters',
-            totalProperty: 'totals'
-        }
-    }
-
-});
+}); 

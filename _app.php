8c384711ae511c8e25bec1f1784f01b9f4cb3fe4
Ernesto J Rodriguez <vela1606@gmail.com>
2012-11-10 20:17:08 -0400

    compiled

--- _app.php
+++ _app.php
@@ -50,7 +50,7 @@ if (!defined('_GaiaEXEC')) die('No direct access allowed.');
 		<!-- slide down message div -->
 		<span id="app-msg" style="display:none;"></span>
 		<!-- Ext library -->
-		<script type="text/javascript" src="lib/extjs-4.1.1a/ext-all-debug.js"></script>
+		<script type="text/javascript" src="lib/extjs-4.1.1a/ext-all.js"></script>
 		<!-- JSrouter and Ext.deirect API files -->
 		<script src="JSrouter.php"></script>
 		<script src="data/api.php"></script>

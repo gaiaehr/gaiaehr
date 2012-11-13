3eede841a22ab4026c2dfe0bef2537ede12441c2
Ernesto J Rodriguez <vela1606@gmail.com>
2012-11-13 02:35:54 -0400

    mobile

--- index.php
+++ index.php
@@ -21,38 +21,38 @@ session_name('GaiaEHR');
 session_start();
 session_cache_limiter('private');
 define('_GaiaEXEC', 1);
-/*
- * Startup the registry
- * This contains SESSION Variables to use in the application
- * and mobile_detect class is used to detect mobile browsers.
- */
-include_once ('registry.php');
 include_once ('classes/Mobile_Detect.php');
-$mobile = new Mobile_Detect();
-/**
- * set the site using the url parameter site, or default if not given
- */
-$site = (isset($_GET['site']) ? $_GET['site'] : 'default');
-if(file_exists('sites/' . $site . '/conf.php')){
-	include_once ('sites/' . $site . '/conf.php');
-	$_SESSION['site']['localization'] = (isset($_SESSION['site']['localization']) && ($_SESSION['site']['default_localization'] != $_SESSION['site']['localization'])) ? $_SESSION['site']['localization'] : 'en_US';
-} else {
-	$_SESSION['site'] = array('error' => 'Site configuration file not found, Please contact Support Desk. Thanks!');
-};
-/**
- * Make the auth process
- * lets check for 4 things to allow the user in
- * 1. $_SESSION['user'] is set (this helps to app clean of PHP NOTICES)
- * 2. $_SESSION['user']['auth'] is true (check if the user is authorized)
- * 3. $_SESSION['user']['site'] is $site ($site == $_GET['site] or 'default')
- * 4. $_SESSION['inactive']['life'] is less than $_SESSION['inactive']['time']
- * (to make sure ths user hasn't been out for a long time)
- *
- */
-$mobileDebug = false;
-if($mobile->isMobile() || $mobileDebug){
+$m = new Mobile_Detect();
+$mDebug = false;
+if($m->isMobile() || $mDebug){
 	header('Location: app_m/');
 }else{
+	/**
+	 * Startup the registry
+	 * This contains SESSION Variables to use in the application
+	 * and mobile_detect class is used to detect mobile browsers.
+	 */
+	include_once ('registry.php');
+	/**
+	 * set the site using the url parameter site, or default if not given
+	 */
+	$site = (isset($_GET['site']) ? $_GET['site'] : 'default');
+	if(file_exists('sites/' . $site . '/conf.php')){
+		include_once ('sites/' . $site . '/conf.php');
+		$_SESSION['site']['localization'] = (isset($_SESSION['site']['localization']) && ($_SESSION['site']['default_localization'] != $_SESSION['site']['localization'])) ? $_SESSION['site']['localization'] : 'en_US';
+	} else {
+		$_SESSION['site'] = array('error' => 'Site configuration file not found, Please contact Support Desk. Thanks!');
+	};
+	/**
+	 * Make the auth process
+	 * lets check for 4 things to allow the user in
+	 * 1. $_SESSION['user'] is set (this helps to app clean of PHP NOTICES)
+	 * 2. $_SESSION['user']['auth'] is true (check if the user is authorized)
+	 * 3. $_SESSION['user']['site'] is $site ($site == $_GET['site] or 'default')
+	 * 4. $_SESSION['inactive']['life'] is less than $_SESSION['inactive']['time']
+	 * (to make sure ths user hasn't been out for a long time)
+	 *
+	 */
 	if(isset($_SESSION['user']) && $_SESSION['user']['auth'] == true && $_SESSION['user']['site'] == $site && $_SESSION['inactive']['life'] < $_SESSION['inactive']['time']){
 		/**
 		 * if mobile go to mobile app, else go to app

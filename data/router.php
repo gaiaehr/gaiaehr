65b455661dfb6d4379949ed3b987e76f1d449b01
Ernesto J Rodriguez <vela1606@gmail.com>
2012-11-13 23:54:26 -0400

    aire progress

--- data/router.php
+++ data/router.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Headers: x-requested-with');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 //----------------------------------------------------------------------------------------------------------------------

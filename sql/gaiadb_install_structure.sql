e41a747987bc05328182d8211b1cdd5130592472
Ernesto J Rodriguez <vela1606@gmail.com>
2013-01-20 17:40:24 -0400

    soap templates / snippets

--- sql/gaiadb_install_structure.sql
+++ sql/gaiadb_install_structure.sql
@@ -2157,6 +2157,16 @@ CREATE TABLE IF NOT EXISTS `sct_relationships` (
   PRIMARY KEY (`RelationshipId`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+CREATE TABLE IF NOT EXISTS `soap_snippets` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `parentId` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `index` int(11) DEFAULT NULL,
+  `text` text COLLATE utf8_bin,
+  `category` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `leaf` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 CREATE TABLE IF NOT EXISTS `standardized_tables_track` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `code_type` varchar(50) DEFAULT NULL,

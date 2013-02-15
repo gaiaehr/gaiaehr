54f9ba82857456e6ab790f87c35853dbfdbc0edf
Gino Rivera Falu <hrivera@gi-technologies.com>
2013-02-15 04:11:14 -0400

    Fixing.

--- classes/dbHelper.php
+++ classes/dbHelper.php
@@ -880,10 +880,8 @@ class dbHelper
 							
 						}
 					}
-
 				}
 			}
-			
 		}
 		catch(PDOException $e)
 		{
@@ -906,17 +904,18 @@ class dbHelper
 			$fileModel = str_replace('.', '/', $fileModel);
 			$senchaModel = (string)file_get_contents($_SESSION['root'] . '/' . $fileModel . '.js');
 
-			// clean comments and un necessary Ext.define functions
-			$senchaModel =  preg_replace("(((/\*(.|\n)*\*/|//(.*))|Ext.define(.*) *|\);)|(\"| |)proxy(.|\n)*},)", '', $senchaModel);
+			// clean comments and unnecessary Ext.define functions
+			$senchaModel =  preg_replace("((/\*(.|\n)*?\*/|//(.*))|([ ](?=(?:[^\'\"]|\'[^\'\"]*\')*$)|\t|\n|\r))", '', $senchaModel);
+			$senchaModel =  preg_replace("(Ext.define\('[A-Za-z0-9.]*',|\);|\"|proxy(.|\n)*},)", '', $senchaModel); //clean coments
 			// wrap with double quotes to all the properties
-			$senchaModel =  preg_replace("/(,|{|\t|\n|\r|  )( |)(\w*):/", "$1$2\"$3\":", $senchaModel);
-			// wrap with double quotes float numbers
-			$senchaModel =  preg_replace("/([0-9]+\.[0-9]+)/", "\"$1\"", $senchaModel);
-			// replace single quotes for double quotes
-			// TODO: refine this to make sure doesn't replace apostrophes used in comments. example: don't
-			$senchaModel =  preg_replace("(')", '"', $senchaModel);
-			// json string => php array
+			$senchaModel =  preg_replace('/(,|\{)(\w*):/', "$1\"$2\":", $senchaModel);
+
+			echo '<pre>';
+			print_r($senchaModel);
+			echo '</pre>';
+			
 			$model = (array)json_decode($senchaModel, true);
+			if(!count($model)) throw new Exception("Ops something when wrong converting it to a array.");
 			
 			// get the table from the model
 			if(!isset($model['table'])) throw new Exception("Table property is not defined on Sencha Model. 'table:'");

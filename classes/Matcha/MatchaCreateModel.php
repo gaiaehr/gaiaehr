7c4ee04e6f3aa93a9abc0b8b0ed70e70e90362f4
Gino Rivera Falu <hrivera@gi-technologies.com>
2013-02-22 04:33:05 -0400

    GaiaEHR: + New class created MatchaCreateModel. This class will generate a Sencha Model .js  and database-table with columns. All this to help Sencha Dynamically created model to communicate with PHP and Database.

--- classes/Matcha/MatchaCreateModel.php
+++ classes/Matcha/MatchaCreateModel.php
@@ -19,19 +19,52 @@
 
 class MatchaCreateModel extends Matcha
 {
-    static public function CreateModelFile($fileModel)
+
+    /**
+     * function MatchaRouter():
+     * Method to serve as router.
+     */
+    public function MatchaRouter()
+    {
+
+    }
+
+    /**
+     * function __createModelFile($fileSenchaModel, $databaseTable = NULL):
+     * Method to create the Sencha Model .js file.
+     * @param $fileSenchaModel
+     * @param null $databaseTable
+     * @return bool
+     * @throws Exception
+     */
+    private function __createModelFile($fileSenchaModel, $databaseTable = NULL)
     {
         try
         {
+            // compose the directory structure
+            $dirLastKey = array_pop(explode('.', $fileSenchaModel));
+            $modelDir = str_replace('.'.$dirLastKey, '', $fileSenchaModel);
+            $modelDir = str_replace('App.', '', $modelDir);
 
-            $file = self::$__app.'/';
+            // check if the directory does not exist, if not create it.
+            if(!opendir( self::$__app.'/'.strtolower(str_replace('.', '/', $modelDir) ) ))
+            {
+                $result = mkdir(self::$__app.'/'.strtolower(str_replace('.', '/', $modelDir) ), 0775, true );
+                if( !$result ) throw new Exception('Could not create the directory.');
+            }
 
-            $file = (string)str_replace('App.', '', $file);
-            $file = str_replace('.', '/', $file);
-            if(!file_exists(self::$__app.'/'.$file.'.'.$type)) throw new Exception('Sencha file "'.self::$__app.'/'.$file.'.'.$type.'" not found.');
-            return (string)file_get_contents(self::$__app.'/'.$file.'.'.$type);
+            // compose the Sencha Model .js for the first time
+            $jsSenchaModel = (string)"Ext.define('".$fileSenchaModel."', {" . chr(13);
+            $jsSenchaModel .= self::t(1)."extend: 'Ext.data.Model'," . chr(13);
+            $jsSenchaModel .= self::t(1)."table: { name:'$databaseTable' },".chr(13);
+            $jsSenchaModel .= self::t(1)."fields: [" . chr(13);
+            $jsSenchaModel .= self::t(1)."{name: 'id', type: 'int'}".chr(13);
+            $jsSenchaModel .= self::t(1)."]" . chr(13);
+            $jsSenchaModel .= '});' . chr(13);
 
-            file_put_contents($file, $jsSenchaModel, LOCK_EX);
+            // create the Sencha Model .js file for the first time
+            $file = self::$__app.'/'.strtolower(str_replace('.', '/', $modelDir) ).'/'.$dirLastKey.'.js';
+            if(!file_put_contents($file, $jsSenchaModel)) throw new Exception('Could not create the Sencha Model file.');
             return true;
         }
         catch(Exception $e)
@@ -40,4 +73,68 @@ class MatchaCreateModel extends Matcha
             return false;
         }
     }
+
+    /**
+     * function __createModelTable($fileSenchaModel, $databaseTable = NULL):
+     * Method to create the model on the table.
+     * @param $fileSenchaModel
+     * @param null $databaseTable
+     * @param array $fields
+     * @return bool
+     */
+    private function __createModelTable($fileSenchaModel, $databaseTable = NULL, $fields = array())
+    {
+        try
+        {
+            $recordSet = self::$__conn->query("SHOW FULL COLUMNS IN ".$databaseTable.";");
+            $tableColumns = $recordSet->fetchAll(PDO::FETCH_ASSOC);
+            $fields = '';
+            foreach($tableColumns as $column) $fields .= self::t(2).self::__renderSenchaFieldSyntax($column).chr(13);
+        }
+        catch(PDOException $e)
+        {
+            MatchaErrorHandler::__errorProcess($e);
+            return false;
+        }
+    }
+
+    /**
+     * function __renderSenchaFieldSyntax($tableColumn):
+     * Method to render the syntax for the Sencha Model fields
+     */
+    private function __renderSenchaFieldSyntax($tableColumn)
+    {
+        $SenchaField = '';
+        switch($tableColumn['Type'])
+        {
+            case 'BIT'; case 'TINYINT'; case 'SMALLINT'; case 'MEDIUMINT'; case 'INT'; case 'INTEGER'; case 'BIGINT':
+                $SenchaType = 'int';
+                break;
+            case 'REAL'; case 'DOUBLE'; case 'FLOAT'; case 'DECIMAL'; case 'NUMERIC':
+                $SenchaType = 'float';
+                break;
+            case 'DATE'; case 'TIME'; case 'TIMESTAMP'; case 'DATETIME'; case 'YEAR':
+                $SenchaType = 'date';
+                break;
+            case 'CHAR'; case 'VARCHAR':
+                $SenchaType = 'string';
+                break;
+        }
+        $SenchaField .= self::t(1)."{name: '".$tableColumn['Field']."', type: '".$SenchaType."'},";
+        return $SenchaField;
+    }
+
+    /**
+     * function t($times = NULL):
+     * Method to product TAB characters
+     * @param null $times
+     * @return string
+     */
+    private function t($times = NULL)
+    {
+        $tabs = '';
+        for ($i = 1; $i <= $times; $i++) $tabs .= chr(9);
+        return $tabs;
+    }
+
 }

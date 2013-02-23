47aaaa06032cc9a463f61a521373e04fd9a8178d
Gino Rivera Falu <hrivera@gi-technologies.com>
2013-02-22 22:12:57 -0400

    Matcha::connect + Adding more comments and also extending the README.md file.

--- classes/Matcha/Matcha.php
+++ classes/Matcha/Matcha.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * Matcha::connect (MatchaCUP Class)
- * Matcha.php
+ * Matcha::connect
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

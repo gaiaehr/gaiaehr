cd51ce227923fe5634dc4f1db5e6288d1b818cfb
Gino Rivera Falu <hrivera@gi-technologies.com>
2013-03-01 00:48:46 -0400

    GaiaEHR : Structure - Created new model and stores for sitesetup.Requirements

--- app/view/sitesetup/SiteSetup.js
+++ app/view/sitesetup/SiteSetup.js
@@ -1,6 +1,21 @@
-/*
- * Site Setup FrontEnd Script 
- * Desc: Script to install GaiaEHR on host.
+/**
+ GaiaEHR (Electronic Health Records)
+ Services.js
+ Store
+ Copyright (C) 2013 Certun
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 Ext.define('App.view.sitesetup.SiteSetup',
@@ -43,32 +58,10 @@ Ext.define('App.view.sitesetup.SiteSetup',
 			 */
 			me.step = [];
 
-			/**
-			 * Store used to load system requirements
-			 */
-			Ext.define('Requirements',
-				{
-					extend:'Ext.data.Model',
-					fields:[
-						{name:'msg', type:'string'},
-						{name:'status', type:'string'}
-					]
-				});
-
 			/*
 			 * Store: requirementsStore
 			 */
-			me.requirementsStore = Ext.create('Ext.data.Store',
-				{
-					model:'Requirements',
-					proxy:{
-						type:'direct',
-						api:{
-							read:SiteSetup.checkRequirements
-						}
-					},
-					autoLoad:false
-				});
+			me.requirementsStore = Ext.create('App.store.sitesetup.Requirements');
 
 			/*
 			 * Store: enviromentStore

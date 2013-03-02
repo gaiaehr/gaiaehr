30353d49dd18a544cece723427e6a6710884c142
Gino Rivera Falu <hrivera@gi-technologies.com>
2013-03-01 20:39:23 -0400

    GaiaEHR : Comments - Added or modified comments on the software license per file. Data is completed.

--- data/appRouter.php
+++ data/appRouter.php
@@ -1,11 +1,22 @@
 <?php
-//header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Headers: origin, x-requested-with, content-type");
-//header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-//----------------------------------------------------------------------------------------------------------------------
-// We sould comment this code and clean it a littler bit.
-// This code was a copy nd paste by Ernesto.
-//----------------------------------------------------------------------------------------------------------------------
+/**
+GaiaEHR (Electronic Health Records)
+Copyright (C) 2013 Certun, inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 if(!isset($_SESSION)){
 	session_name('GaiaEHR');
 	session_start();

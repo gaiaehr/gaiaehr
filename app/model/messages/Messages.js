59e446d0be4fa7b9d9e4687109ae21e54523e23a
Gino Rivera Falu <hrivera@gi-technologies.com>
2013-03-09 15:22:53 -0400

    GaiaEHR: (messages) - Eliminating the extra table properties from the sencha models, is no longer needed. Some of the properties are set as default by Matcha.

--- app/model/messages/Messages.js
+++ app/model/messages/Messages.js
@@ -21,10 +21,6 @@ Ext.define('App.model.messages.Messages',
     extend : 'Ext.data.Model',
     table: {
         name:'messages',
-        engine:'InnoDB',
-        autoIncrement:1,
-        charset:'utf8',
-        collate:'utf8_bin',
         comment:'Messages'
     },
     fields : [

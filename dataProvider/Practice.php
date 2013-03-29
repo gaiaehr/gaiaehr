df53d34099f4aea571ae6e6d44a6ac9b02d60014
Gino Rivera Falu <hrivera@gi-technologies.com>
2013-03-28 23:17:23 -0400

    GaiaEHR: - Converting Practice DataProvider to use Matcha::connect.

--- dataProvider/Practice.php
+++ dataProvider/Practice.php
@@ -47,11 +47,9 @@ class Practice extends MatchaHelper
     //------------------------------------------------------------------------------------------------------------------
     public function getPharmacies()
     {
+        $rows = array();
         if($this->Pharmacy == NULL) $this->Pharmacy = MatchaModel::setSenchaModel('App.model.Pharmacy');
         if($this->Phone == NULL) $this->Phone = MatchaModel::setSenchaModel('App.model.administration.Phone');
-
-        $this->Phone = MatchaModel::setSenchaModel('App.model.administration.Phone');
-        $rows = array();
         foreach($this->Pharmacy->load()->all() as $row)
         {
             $row['address_full'] = $row['line1'].' '.$row['line2'].' '.$row['city'].','.$row['state'].' '.$row['zip'].'-'.$row['plus_four'].' '.$row['country'];
@@ -63,21 +61,16 @@ class Practice extends MatchaHelper
     public function addPharmacy(stdClass $params)
     {
         if($this->Pharmacy == NULL) $this->Pharmacy = MatchaModel::setSenchaModel('App.model.Pharmacy');
-        if($this->Insurance == NULL) $this->Insurance = MatchaModel::setSenchaModel('App.model.administration.Insurance');
-
-        $params->id = $this->Insurance()->nextId();
+        $params->id = $this->getNextPharmacyInsuranceId();
         $data = get_object_vars($params);
         $row['id'] = $data['id'];
         $row['name'] = $data['name'];
         $row['transmit_method'] = $data['transmit_method'];
         $row['email'] = $data['email'];
         $row['active'] = $data['active'];
-        $sql = $this->sqlBind($row, 'pharmacies', 'I');
-        $this->setSQL($sql);
-        $this->execLog();
+        $this->Pharmacy->save($row);
         $params = $this->addAddress($params);
         $params = $this->addPhones($params);
-
         return $params;
     }
 
@@ -89,9 +82,7 @@ class Practice extends MatchaHelper
         $row['transmit_method'] = $data['transmit_method'];
         $row['email'] = $data['email'];
         $row['active'] = $data['active'];
-        $sql = $this->sqlBind($row, 'pharmacies', 'U', array('id' => $data['id']));
-        $this->setSQL($sql);
-        $this->execLog();
+        $this->Pharmacy->save($row);
         $params = $this->updateAddress($params);
         $params = $this->updatePhones($params);
         return $params;
@@ -100,27 +91,12 @@ class Practice extends MatchaHelper
     public function getLaboratories()
     {
         if($this->Laboratory == NULL) $this->Laboratory = MatchaModel::setSenchaModel('App.model.administration.Laboratories');
-        if($this->Phone == NULL) $this->Phone = MatchaModel::setSenchaModel('App.model.administration.Phone');
+        if($this->Address == NULL) $this->Address = MatchaModel::setSenchaModel('App.model.administration.Address');
         $rows = array();
-        $this->setSQL("SELECT p.id AS id,
-                              p.name,
-                              p.transmit_method,
-                              p.email,
-                              p.active,
-                              a.id AS address_id,
-                              a.line1,
-                              a.line2,
-                              a.city,
-                              a.state,
-                              a.zip,
-                              a.plus_four,
-                              a.country,
-                              a.foreign_id AS address_foreign_id
-                         FROM laboratories AS p
-                    LEFT JOIN addresses AS a ON p.id = a.foreign_id
-                     ORDER BY p.name DESC");
-        foreach ($this->fetchRecords(PDO::FETCH_ASSOC) as $row) {
-            $this->setSQL("SELECT * FROM phone_numbers WHERE phone_numbers.foreign_id =" . $row['id'] . "");
+        foreach ($this->Laboratory->load()->all() as $row)
+        {
+            $address = $this->Address->load(array('foreign_id'=>$row['id']), array('address_id', 'line1', 'line2', 'city', 'state', 'zip', 'plus_four', 'country'));
+            array_push($rows, $address);
             $row = $this->getPhones($row);
             $row['address_full'] = $row['line1'] . ' ' . $row['line2'] . ' ' . $row['city'] . ',' . $row['state'] . ' ' . $row['zip'] . '-' . $row['plus_four'] . ' ' . $row['country'];
             array_push($rows, $row);
@@ -131,7 +107,6 @@ class Practice extends MatchaHelper
     public function addLaboratory(stdClass $params)
     {
         if($this->Laboratory == NULL) $this->Laboratory = MatchaModel::setSenchaModel('App.model.administration.Laboratories');
-
         $params->id = $this->getNextPharmacyInsuranceId();
         $data = get_object_vars($params);
         $row['id'] = $data['id'];
@@ -139,27 +114,21 @@ class Practice extends MatchaHelper
         $row['transmit_method'] = $data['transmit_method'];
         $row['email'] = $data['email'];
         $row['active'] = $data['active'];
-        $sql = $this->sqlBind($row, 'laboratories', 'I');
-        $this->setSQL($sql);
-        $this->execLog();
+        $this->Laboratory->save($row);
         $params = $this->addAddress($params);
         $params = $this->addPhones($params);
-
         return $params;
     }
 
     public function updateLaboratory(stdClass $params)
     {
         if($this->Laboratory == NULL) $this->Laboratory = MatchaModel::setSenchaModel('App.model.administration.Laboratories');
-
         $data = get_object_vars($params);
         $row['name'] = $data['name'];
         $row['transmit_method'] = $data['transmit_method'];
         $row['email'] = $data['email'];
         $row['active'] = $data['active'];
-        $sql = $this->sqlBind($row, 'laboratories', 'U', array('id' => $data['id']));
-        $this->setSQL($sql);
-        $this->execLog();
+        $this->Laboratory->save($row);
         $params = $this->updateAddress($params);
         $params = $this->updatePhones($params);
         return $params;
@@ -168,30 +137,12 @@ class Practice extends MatchaHelper
     public function getInsurances()
     {
         if($this->Insurance == NULL) $this->Insurance = MatchaModel::setSenchaModel('App.model.administration.Insurance');
+        if($this->Address == NULL) $this->Address = MatchaModel::setSenchaModel('App.model.administration.Address');
         $rows = array();
-        $this->setSQL("SELECT i.id AS id,
-                              i.name,
-                              i.attn,
-                              i.cms_id,
-                              i.freeb_type,
-                              i.x12_receiver_id,
-                              i.x12_default_partner_id,
-                              i.active,
-                              i.alt_cms_id,
-                              a.id AS address_id,
-                              a.line1,
-                              a.line2,
-                              a.city,
-                              a.state,
-                              a.zip,
-                              a.plus_four,
-                              a.country,
-                              a.foreign_id AS address_foreign_id
-                         FROM insurance_companies AS i
-                    LEFT JOIN addresses AS a ON i.id = a.foreign_id
-                     ORDER BY i.name DESC");
-        foreach($this->fetchRecords(PDO::FETCH_ASSOC) as $row) {
-            $this->setSQL("SELECT * FROM phone_numbers WHERE phone_numbers.foreign_id =" . $row['id'] . "");
+        foreach($this->Insurance->load()->all() as $row)
+        {
+            $address = $this->Address->load(array('foreign_id'=>$row['id']), array('address_id', 'line1', 'line2', 'city', 'state', 'zip', 'plus_four', 'country'));
+            array_push($rows, $address);
             $row = $this->getPhones($row);
             $row['address_full'] = $row['line1'] . ' ' . $row['line2'] . ' ' . $row['city'] . ',' . $row['state'] . ' ' . $row['zip'] . '-' . $row['plus_four'] . ' ' . $row['country'];
             array_push($rows, $row);
@@ -213,9 +164,7 @@ class Practice extends MatchaHelper
         $row['x12_default_partner_id'] = $data['x12_default_partner_id'];
         $row['alt_cms_id'] = $data['alt_cms_id'];
         $row['active'] = $data['active'];
-        $sql = $this->sqlBind($row, 'insurance_companies', 'I');
-        $this->setSQL($sql);
-        $this->execLog();
+        $this->Insurance->save($row);
         $params = $this->addAddress($params);
         $params = $this->addPhones($params);
         return $params;
@@ -233,9 +182,7 @@ class Practice extends MatchaHelper
         $row['x12_default_partner_id'] = $data['x12_default_partner_id'];
         $row['alt_cms_id'] = $data['alt_cms_id'];
         $row['active'] = $data['active'];
-        $sql = $this->sqlBind($row, 'insurance_companies', 'U', array('id' => $data['id']));
-        $this->setSQL($sql);
-        $this->execLog();
+        $this->Insurance->save($row);
         $params = $this->updateAddress($params);
         $params = $this->updatePhones($params);
         return $params;
@@ -263,9 +210,7 @@ class Practice extends MatchaHelper
         $arow['plus_four'] = $data['plus_four'];
         $arow['country'] = $data['country'];
         $arow['foreign_id'] = $data['id'];
-        $sql = $this->sqlBind($arow, 'addresses', 'I');
-        $this->setSQL($sql);
-        $this->execOnly();
+        $this->Address->save($arow);
         $params->address_full = $params->line1 . ' ' . $params->line2 . ' ' . $params->city . ',' . $params->state . ' ' . $params->zip . '-' . $params->plus_four . ' ' . $params->country;
         return $params;
     }
@@ -281,9 +226,7 @@ class Practice extends MatchaHelper
         $arow['zip'] = $data['zip'];
         $arow['plus_four'] = $data['plus_four'];
         $arow['country'] = $data['country'];
-        $sql = $this->sqlBind($arow, 'addresses', 'U', array('foreign_id' => $data['id']));
-        $this->setSQL($sql);
-        $this->execLog();
+        $this->Address->save($arow);
         $params->address_full = $params->line1 . ' ' . $params->line2 . ' ' . $params->city . ',' . $params->state . ' ' . $params->zip . '-' . $params->plus_four . ' ' . $params->country;
         return $params;
     }
@@ -291,9 +234,10 @@ class Practice extends MatchaHelper
     private function getPhones($row)
     {
         if($this->Phone == NULL) $this->Phone = MatchaModel::setSenchaModel('App.model.administration.Phone');
-        $this->setSQL("SELECT * FROM phone_numbers WHERE phone_numbers.foreign_id =" . $row['id'] . "");
-        foreach ($this->fetchRecords(PDO::FETCH_ASSOC) as $phoneRow) {
-            switch ($phoneRow['type']) {
+        foreach ($this->Phone->load(array('foreign_id'=>$row['id'])) as $phoneRow)
+        {
+            switch ($phoneRow['type'])
+            {
                 case "2" :
                     $row['phone_id'] = $phoneRow['id'];
                     $row['phone_country_code'] = $phoneRow['country_code'];
@@ -331,16 +275,10 @@ class Practice extends MatchaHelper
         $frow['number'] = $data['fax_number'];
         $frow['type'] = 5;
         $frow['foreign_id'] = $data['id'];
-        $sql = $this->sqlBind($prow, 'phone_numbers', 'I');
-        $this->setSQL($sql);
-        $this->execOnly();
-        $sql = $this->sqlBind($frow, 'phone_numbers', 'I');
-        $this->setSQL($sql);
-        $this->execOnly();
-
+        $this->Phone->save($prow);
+        $this->Phone->save($frow);
         $params->phone_full = $prow['country_code'] . ' ' . $prow['area_code'] . '-' . $prow['prefix'] . '-' . $prow['number'];
         $params->fax_full = $frow['country_code'] . ' ' . $frow['area_code'] . '-' . $frow['prefix'] . '-' . $frow['number'];
-
         return $params;
     }
 
@@ -348,43 +286,33 @@ class Practice extends MatchaHelper
     {
         if($this->Phone == NULL) $this->Phone = MatchaModel::setSenchaModel('App.model.administration.Phone');
         $data = get_object_vars($params);
-
+        $prow['foreign_id'] =  $data['id'];
         $prow['country_code'] = $data['phone_country_code'];
         $prow['area_code'] = $data['phone_area_code'];
         $prow['prefix'] = $data['phone_prefix'];
         $prow['number'] = $data['phone_number'];
-
+        $prow['type'] = 2;
+        $frow['foreign_id'] =  $data['id'];
         $frow['country_code'] = $data['fax_country_code'];
         $frow['area_code'] = $data['fax_area_code'];
         $frow['prefix'] = $data['fax_prefix'];
         $frow['number'] = $data['fax_number'];
-        $sql = $this->sqlBind($prow, 'phone_numbers', 'U', array(
-            'foreign_id' => $data['id'],
-            'type' => 2
-        ));
-        $this->setSQL($sql);
-        $this->execLog();
-        $sql = $this->sqlBind($frow, 'phone_numbers', 'U', array(
-            'foreign_id' => $data['id'],
-            'type' => 5
-        ));
-        $this->setSQL($sql);
-        $this->execLog();
-
+        $prow['type'] = 5;
+        $this->Phone->save($prow);
+        $this->Phone->save($frow);
         $params->phone_full = $prow['country_code'] . ' ' . $prow['area_code'] . '-' . $prow['prefix'] . '-' . $prow['number'];
         $params->fax_full = $frow['country_code'] . ' ' . $frow['area_code'] . '-' . $frow['prefix'] . '-' . $frow['number'];
-
         return $params;
     }
 
     private function getNextPharmacyInsuranceId()
     {
-        $this->setSQL("SELECT MAX(id) AS maxid FROM pharmacies");
-        $prec = $this->fetchRecord();
-        $this->setSQL("SELECT MAX(id) AS maxid FROM laboratories");
-        $lrec = $this->fetchRecord();
-        $this->setSQL("SELECT MAX(id) AS maxid FROM insurance_companies");
-        $irec = $this->fetchRecord();
+        if($this->Pharmacy == NULL) $this->Pharmacy = MatchaModel::setSenchaModel('App.model.Pharmacy');
+        if($this->Laboratory == NULL) $this->Laboratory = MatchaModel::setSenchaModel('App.model.administration.Laboratories');
+        if($this->Insurance == NULL) $this->Insurance = MatchaModel::setSenchaModel('App.model.administration.Insurance');
+        $prec = $this->Pharmacy->nextId();
+        $lrec = $this->Laboratory->nextId();
+        $irec = $this->Insurance->nextId();
         return max($prec['maxid'], $lrec['maxid'], $irec['maxid']) + 1;
     }
 

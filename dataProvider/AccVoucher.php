afabefeb0f9bae2cefc838cc6aea185be21396df
Gino Rivera Falu <hrivera@gi-technologies.com>
2013-03-29 10:48:47 -0400

    GaiaEHR: - AccVoucher dataProvider using Matcha::connect partially.

--- dataProvider/AccVoucher.php
+++ dataProvider/AccVoucher.php
@@ -21,30 +21,38 @@ include_once ($_SESSION['root'] . '/dataProvider/AccAccount.php');
 
 class AccVoucher extends AccAccount {
 
-	private $voucher;
-	private $voucherLine;
+    /**
+     * MatchaCup
+     */
+	private $voucher = NULL;
+	private $voucherLine = NULL;
 
     function __construct()
     {
 	    parent::__construct();
-	    $this->voucher      = Matcha::setSenchaModel('App.model.account.Voucher');
-	    $this->voucherLine  = Matcha::setSenchaModel('App.model.account.VoucherLine');
+	    if($this->voucher == NULL) $this->voucher = Matcha::setSenchaModel('App.model.account.Voucher');
+	    if($this->voucherLine == NULL) $this->voucherLine = Matcha::setSenchaModel('App.model.account.VoucherLine');
+        return;
     }
 
 	/**
 	 * Voucher
 	 */
-	public function getVoucher($params){
+	public function getVoucher($params)
+    {
 		return $this->voucher->load($params)->one();
     }
-    public function createVoucher($params){
+    public function createVoucher($params)
+    {
+
 	    $data = get_object_vars($params);
 	    $this->db->setSQL($this->db->sqlBind($data,'accvoucher','I'));
 	    $this->db->execLog();
 	    $params->id = $this->db->lastInsertId;
 	    return $params;
     }
-    public function updateVoucher($params){
+    public function updateVoucher($params)
+    {
 	    $data = get_object_vars($params);
 	    unset($data['id']);
 	    $this->db->setSQL($this->db->sqlBind($data,'accvoucher','U', array('id'=>$params->id)));
@@ -60,26 +68,28 @@ class AccVoucher extends AccAccount {
 	/**
 	 * Voucher Lines
 	 */
-    public function getVoucherLines($params){
-	    $this->db->setSQL("SELECT * FROM `accvoucherline` WHERE voucherId = '$params->voucherId'");
-	    return $this->db->fetchRecords(PDO::FETCH_ASSOC);
+    public function getVoucherLines($params)
+    {
+        return $this->voucherLine->load($params);
     }
-    public function createVoucherLine($params){
+    public function createVoucherLine($params)
+    {
 	    $data = get_object_vars($params);
 	    $this->db->setSQL($this->db->sqlBind($data,'accvoucherline','I'));
 	    $this->db->execLog();
 	    $params->id = $this->db->lastInsertId;
 	    return $params;
     }
-    public function updateVoucherLine($params){
+    public function updateVoucherLine($params)
+    {
 	    $data = get_object_vars($params);
 	    unset($data['id']);
 	    $this->db->setSQL($this->db->sqlBind($data,'accvoucherline','U', array('id'=>$params->id)));
 	    $this->db->execLog();
 	    return $params;
     }
-    public function destroyVoucherLine($params){
-
+    public function destroyVoucherLine($params)
+    {
 	    return $params;
     }
 }

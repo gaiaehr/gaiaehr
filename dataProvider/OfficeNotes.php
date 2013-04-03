37bb31c95da848d1991953e9139651d9877396bf
Gino Rivera Falu <hrivera@gi-technologies.com>
2013-04-03 16:02:21 -0400

    GaiaEHR: - Office Notes, model and getOfficeNotes fixed. I was not calling the entire method from Matcha.

--- dataProvider/OfficeNotes.php
+++ dataProvider/OfficeNotes.php
@@ -26,7 +26,7 @@ class OfficeNotes extends MatchaHelper
 
     function __construct()
     {
-        if($this->OfficeNotes = NULL) $this->OfficeNotes = MatchaModel::setSenchaModel('App.model.miscellaneous.OfficeNotes');
+        if($this->OfficeNotes == NULL) $this->OfficeNotes = MatchaModel::setSenchaModel('App.model.miscellaneous.OfficeNotes');
         return;
     }
 

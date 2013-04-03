fe330051a7883ea4b693d6b1e24f1925757a2a0d
Gino Rivera Falu <hrivera@gi-technologies.com>
2013-04-03 19:45:17 -0400

    GaiaEHR: -Now logs uses Matcha::connect.

--- dataProvider/Logs.php
+++ dataProvider/Logs.php
@@ -20,30 +20,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 class Logs extends MatchaHelper
 {
 
-	/**
-	 * @brief     Get all logs
-	 * @details   This method will return all the logs and filter them by start and
-	 * limit
-	 *            to be used with the Ext.ux.SlidingPager plugin.
-	 *
-	 * @author    Ernesto J. Rodriguez (Certun) <erodriguez@certun.com>
-	 * @version   Vega 1.0
-	 *
-	 * @warning   getLogs 'len' in /data/cinfig.php must be set to 1 in order for
-	 * Ext.direct to send the Params
-	 *
-	 * @param stdClass $params Params sent from sencha
-	 * @return mixed array of records with totals count
-	 */
+    /**
+     * Data Object
+     */
+    private $Log = NULL;
+
+    function __construct()
+    {
+        if($this->Log == NULL) $this->Log = MatchaModel::setSenchaModel('App.model.administration.Logs');
+        return;
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    // Main Sencha Model Getter and Setters
+    //------------------------------------------------------------------------------------------------------------------
 	public function getLogs(stdClass $params)
 	{
-		$this -> setSQL("SELECT * FROM log ORDER BY id DESC");
-		$records = $this -> fetchRecords(PDO::FETCH_CLASS);
+		$records = $this->Log->load($params)->all();
 		$total = count($records);
-		$rows = array_splice($records, $params -> start, $params -> limit);
 		return array(
 			'totals' => $total,
-			'rows' => $rows
+			'rows' => $records
 		);
 	}
 

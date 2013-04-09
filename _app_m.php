b6eb606eac4dc89a8edb66ae511779b606cc4017
Gino Rivera Falu <hrivera@gi-technologies.com>
2013-04-09 18:15:34 -0400

    GaiaEHR: - Commenting files about GPL, and organizing code.

--- _app_m.php
+++ _app_m.php
@@ -1,21 +1,22 @@
 <?php
-/* Main Screen Application
+/**
+ * GaiaEHR
  *
- * Description: This is the main application, with all the panels
- * also this is the viewport of the application, this will call
- * all the app->screen panels
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * version 0.0.3
- * revision: N/A
- * author: GI Technologies, 2011
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-// Reset session count
+
 $_SESSION['site']['flops'] = 0;
-/*
- * Include the necessary libraries, so the web application
- * can work.
- */
 ?>
 <!DOCTYPE html>
 <html>

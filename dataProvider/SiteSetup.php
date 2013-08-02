3a3a8e79198044d01fa7f16f5f19d8093b33cf6f
Ernesto J Rodriguez <vela1606@gmail.com>
2013-08-02 14:14:21 -0400

    reports all php error during installation

--- dataProvider/SiteSetup.php
+++ dataProvider/SiteSetup.php
@@ -16,7 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+error_reporting(E_ALL);
 include_once ($_SESSION['root'] . '/classes/FileManager.php');
 include_once ($_SESSION['root'] . '/dataProvider/ACL.php');
 

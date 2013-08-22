e3b0263e46b1e1ba0d54404b2b6725d037fbb1a0
Gino Rivera <hrivera@gi-technologies.com>
2013-08-22 14:42:25 -0400

    170.304.h Clinical Summaries - comments

--- dataProvider/CCDdocument.php
+++ dataProvider/CCDdocument.php
@@ -1,23 +1,12 @@
 <?php
 /**
- * This code was originally create by Garden State Health Systems for OpenEMR
- * (see credits bellow) and heavily modified by Ernesto J Rodriguez to make it
- * work with GaiaEHR class system.
+ * GaiaEHR (Electronic Health Records)
+ * Copyright (C) 2013 Certun, inc.
  *
- * ------------------------------------------------------------------------
- *                     Garden State Health Systems
- *                    Copyright (c) 2010 gshsys.com
- *                      <http://www.gshsys.com/>
- * ------------------------------------------------------------------------
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
- * You may not change or alter any portion of this comment or credits
- * of supporting developers from this source code or any supporting
- * source code which is considered copyrighted (c) material of the
- * original comment or credit authors.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -25,9 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 if (!isset($_SESSION))

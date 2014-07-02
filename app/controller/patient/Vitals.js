a623a1dacbfbc73091a45072e6cf247d18a61bd8
Ernesto J Rodriguez <vela1606@gmail.com>
2014-07-02 14:55:04 -0400

    GAIAEH-356 Metric Values fixed

--- app/controller/patient/Vitals.js
+++ app/controller/patient/Vitals.js
@@ -261,18 +261,31 @@ Ext.define('App.controller.patient.Vitals', {
 	},
 
 	doUpdateBlocks: function(records){
+		var me = this;
 		if(records.length > 0){
 			this.getBpBlock().update(this.getBlockTemplate('bp', records[0]));
-			this.getTempBlock().update(this.getBlockTemplate('temp_f', records[0]));
-			this.getWeighBlock().update(this.getBlockTemplate('weight_lbs', records[0]));
-			this.getHeightBlock().update(this.getBlockTemplate('height_in', records[0]));
+			if(me.isMetric()){
+				this.getTempBlock().update(this.getBlockTemplate('temp_c', records[0]));
+				this.getWeighBlock().update(this.getBlockTemplate('weight_kg', records[0]));
+				this.getHeightBlock().update(this.getBlockTemplate('height_cm', records[0]));
+			}else{
+				this.getTempBlock().update(this.getBlockTemplate('temp_f', records[0]));
+				this.getWeighBlock().update(this.getBlockTemplate('weight_lbs', records[0]));
+				this.getHeightBlock().update(this.getBlockTemplate('height_in', records[0]));
+			}
 			this.getBmiBlock().update(this.getBlockTemplate('bmi', records[0]));
 			this.getNotesBlock().update(this.getBlockTemplate('other_notes', records[0]));
 		}else{
 			this.getBpBlock().update(this.getBlockTemplate('bp', false));
-			this.getTempBlock().update(this.getBlockTemplate('temp_f', false));
-			this.getWeighBlock().update(this.getBlockTemplate('weight_lbs', false));
-			this.getHeightBlock().update(this.getBlockTemplate('height_in', false));
+			if(me.isMetric()){
+				this.getTempBlock().update(this.getBlockTemplate('temp_c', false));
+				this.getWeighBlock().update(this.getBlockTemplate('weight_kg', false));
+				this.getHeightBlock().update(this.getBlockTemplate('height_cm', false));
+			}else{
+				this.getTempBlock().update(this.getBlockTemplate('temp_f', false));
+				this.getWeighBlock().update(this.getBlockTemplate('weight_lbs', false));
+				this.getHeightBlock().update(this.getBlockTemplate('height_in', false));
+			}
 			this.getBmiBlock().update(this.getBlockTemplate('bmi', false));
 			this.getNotesBlock().update(this.getBlockTemplate('other_notes', false));
 		}

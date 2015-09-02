7d8e261ea80ee5d932bdf373e44080db2ba1da60
Gino Rivera <hrivera@tranextgen.com>
2015-09-02 12:35:51 -0400

    General: - Fixed typo in WHERE clause. (Services dataProvider)

--- dataProvider/Services.php
+++ dataProvider/Services.php
@@ -42,7 +42,7 @@ class Services {
             FROM encounter_services as ES
             LEFT JOIN combo_lists_options as CLO
             ON (ES.financial_class = CLO.option_value) AND (CLO.list_id=135)
-            WHEREhr ES.eid=".$params->filter[0]->value)->all();
+            WHERE ES.eid=".$params->filter[0]->value)->all();
 	}
 
 	public function getEncounterService($params){

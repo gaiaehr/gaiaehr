a20a312ed55f2933032ecb5d4e7573af398c40e9
Ernesto J Rodriguez <vela1606@gmail.com>
2015-09-10 11:55:53 -0400

    Read Only Label Plugin

---
+++ app/ux/form/fields/plugin/ReadOnlyLabel.js
@@ -0,0 +1,64 @@
+Ext.define('App.ux.form.fields.plugin.ReadOnlyLabel', {
+	extend: 'Ext.AbstractPlugin',
+	alias: 'plugin.readonlylabel',
+
+	align: 'right',
+
+	text: _('read_only'),
+	textBg: 'red',
+	textSize: '14',
+	textColor: 'white',
+	textOpacity: .7,
+
+	/**
+	 *
+	 * @param field
+	 */
+	init: function(field){
+		var  me = this;
+
+		field.on('render', me.onRender, me);
+		field.on('writeablechange', me.setReadOnly, me);
+
+	},
+
+	setReadOnly: function(field, readOnly){
+		field.readOnlyEl.setVisible(readOnly);
+	},
+
+	onRender: function(field){
+		this.addReadOnlyEl(field);
+	},
+
+	/**
+	 *
+	 * @param field
+	 */
+	addReadOnlyEl: function(field){
+		var me = this,
+			styles = {
+				'position': 'absolute',
+				'background-color': me.textBg,
+				'font-size': me.textSize + 'px',
+				'color': me.textColor,
+				'padding': '5px 10px',
+				'index': 50,
+				'top': '10px',
+				'border-radius': '15px',
+				'visibility': 'hidden'
+			};
+
+		if(me.align == 'left'){
+			styles.left = '10px';
+		}else{
+			styles.right = '10px';
+		}
+
+		field.readOnlyEl = Ext.DomHelper.append(field.el, { tag:'div', cls:'badgeText x-unselectable'}, true);
+		field.readOnlyEl.setOpacity(me.textOpacity);
+		field.readOnlyEl.setStyle(styles);
+		field.readOnlyEl.update(me.text.toString ? me.text.toString() : me.text);
+
+	}
+
+});

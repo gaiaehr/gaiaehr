e918b27ea4a54533bbff91eb46b3dfb4daa6af0e
Gino Rivera <hrivera@tranextgen.com>
2015-09-24 01:42:48 -0400

    General: - Fixing getSelfContact getters

--- dataProvider/Patient.php
+++ dataProvider/Patient.php
@@ -440,13 +440,15 @@ class Patient {
 	public function getPatientFullAddressByPid($pid) {
         $patientContact = new PatientContacts();
         $record = $patientContact->getSelfContact($pid);
-		return Person::fulladdress(
-            $record['street_mailing_address'],
-            null,
-            $record['city'],
-            $record['state'],
-            $record['zip']
-        );
+        if(isset($record)) {
+            return Person::fulladdress(
+                $record['street_mailing_address'],
+                null,
+                $record[0]['city'],
+                $record[0]['state'],
+                $record[0]['zip']
+            );
+        }
 	}
 
 	public function patientLiveSearch(stdClass $params) {
@@ -508,7 +510,10 @@ class Patient {
 	public function getPatientAddressById($pid) {
         $patientContact = new PatientContacts();
         $record = $patientContact->getSelfContact($pid);
-		$address = $record['address'] . ' <br>' . $record['city'] . ',  ' . $record['state'] . ' ' . $record['country'];
+        $address = '';
+        if(isset($record)) {
+            $address = $record[0]['address'].' <br>'.$record[0]['city'].',  '.$record[0]['state'].' '.$record[0]['country'];
+        }
 		return $address;
 	}
 
@@ -720,22 +725,26 @@ class Patient {
 
 		$results = $this->p->sql($sql)->all();
         foreach($results as $index => $record ){
-            $contact = $this->patientContacts->getSelfContact($record['pid'])[0];
+            $contact = $this->patientContacts->getSelfContact($record['pid']);
             $results[$index]['name'] = Person::fullname(
                 $record['fname'],
                 $record['mname'],
                 $record['lname']
             );
-            $results[$index]['fulladdress'] = Person::fulladdress(
-                isset($contact['street_mailing_address']) ? $contact['street_mailing_address'] : '',
-                null,
-                isset($contact['city']) ? $contact['city'] : '',
-                isset($contact['state']) ? $contact['state'] : '',
-                isset($contact['zip']) ? $contact['zip'] : ''
-            );
-            $results[$index]['phones'] = isset($contact['phone_local_number']) ?
-                $contact['phone_use_code'].'-'.$contact['phone_area_code'].'-'.$contact['phone_local_number'] :
-                '';
+            if(isset($contact)) {
+                $results[$index]['fulladdress'] = Person::fulladdress(
+                    isset($contact[0]['street_mailing_address']) ? $contact[0]['street_mailing_address'] : '',
+                    null,
+                    isset($contact[0]['city']) ? $contact[0]['city'] : '',
+                    isset($contact[0]['state']) ? $contact[0]['state'] : '',
+                    isset($contact[0]['zip']) ? $contact[0]['zip'] : ''
+                );
+                $results[$index]['phones'] = isset($contact[0]['phone_local_number'])
+                    ?
+                    $contact[0]['phone_use_code'] . '-' . $contact[0]['phone_area_code'] . '-' . $contact[0]['phone_local_number']
+                    :
+                    '';
+            }
         }
 		return [
             'total' => count($results),

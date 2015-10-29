c0357f3b5d7504253b522e8fac8a3be7b7c3c5d7
Gino Rivera <hrivera@tranextgen.com>
2015-10-29 16:44:47 -0400

    Report Center: (Patient List) - Building the SQL Statement for Patient List NOTE: Still work in progress

--- modules/reportcenter/reports/PatientList/1on1_BuildUp.sql
+++ modules/reportcenter/reports/PatientList/1on1_BuildUp.sql
@@ -1,38 +1,22 @@
 -- Set all the variables
 -- SET @Provider = 6;
 SET @Provider = null;
+-- SET @Provider = 3;
 -- SET @StartDate = '2015-01-01';
 -- SET @EndDate = '2015-12-31';
 -- SET @ProblemCode = '195967001';
-SET @ProblemCode = null;
+-- SET @ProblemCode = null;
 
 -- Display all the patient fields
 SELECT patient.* 
 FROM patient
 
--- Join the Active Problems
-LEFT JOIN (
-SELECT distinct(pid) AS pid, code
-	FROM patient_active_problems
-	LIMIT 1
-) patient_active_problems ON patient.pid = patient_active_problems.pid
-
--- Join the Encounters
 LEFT JOIN (
 SELECT distinct(pid) AS pid, provider_uid
 	FROM encounters
 ) encounters ON patient.pid = encounters.pid
 
-
--- Filter by Patient Active Problems
 WHERE CASE 
-	WHEN @ProblemCode IS NOT NULL 
-	THEN patient_active_problems.code = @ProblemCode 
-	ELSE 1=1 
-END
-
--- Filter by Provider
-AND CASE 
 	WHEN @Provider IS NOT NULL 
 	THEN encounters.provider_uid = @Provider 
 	ELSE 1=1 

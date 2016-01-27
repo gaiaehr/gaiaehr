cb5d378d0a847393f814cbe882ff64f4127359a5
Ernesto J Rodriguez <vela1606@gmail.com>
2016-01-26 23:30:54 -0400

    document encryption and memory

--- dataProvider/DocumentViewer.php
+++ dataProvider/DocumentViewer.php
@@ -28,7 +28,7 @@ if(!defined('_GaiaEXEC')) define('_GaiaEXEC', 1);
 require_once(str_replace('\\', '/', dirname(dirname(__FILE__))) . '/registry.php');
 require_once(ROOT . '/sites/'. $_REQUEST['site'] .'/conf.php');
 
-ini_set('memory_limit', '256M');
+ini_set('memory_limit', '1024M');
 ini_set('max_execution_time', 5);
 
 if(isset($_SESSION['user']) && $_SESSION['user']['auth'] == true){
@@ -81,7 +81,7 @@ if(isset($_SESSION['user']) && $_SESSION['user']['auth'] == true){
 
 	function base64ToBinary($document, $encrypted, $is_image){
 		if($encrypted == true){
-			$document = base64_decode(MatchaUtils::decrypt($document));
+			$document = MatchaUtils::decrypt($document);
 		}
 
 		if(!$is_image){

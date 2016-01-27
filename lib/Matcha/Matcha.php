03f2e39ee58b37f117f77c939a64b468e30e55e8
Gino Rivera <hrivera@tranextgen.com>
2016-01-27 10:15:07 -0400

    Matcha::Connect - Validating for PDO::MYSQL_ATTR_MAX_BUFFER_SIZE in MySQL if it has it increase the size.

--- lib/Matcha/Matcha.php
+++ lib/Matcha/Matcha.php
@@ -98,7 +98,13 @@ class Matcha {
 					PDO::ATTR_PERSISTENT => false,
 				));
 
-				self::$__conn->setAttribute(PDO::MYSQL_ATTR_MAX_BUFFER_SIZE, 1024*1024*100); //100MB
+                // Check if the current version of PDO::MySQL has this parameter available
+                // If not activate it.
+                if( defined( 'PDO::MYSQL_ATTR_MAX_BUFFER_SIZE' ) ) // note the quotes
+                {
+                    // Increase the size to 100MB
+                    self::$__conn->setAttribute(PDO::MYSQL_ATTR_MAX_BUFFER_SIZE, 1024 * 1024 * 100);
+                }
 				self::$__conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				self::$__conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 				self::$__conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);

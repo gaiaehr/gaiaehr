f0befab31277d140ac1a3f94f091fc3f2fd1ad45
Gino Rivera <hrivera@tranextgen.com>
2016-02-01 12:57:53 -0400

    Debugging...

--- modules/reportcenter/dataProvider/ReportGenerator.php
+++ modules/reportcenter/dataProvider/ReportGenerator.php
@@ -103,6 +103,7 @@ class ReportGenerator
                 $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
 
                 // Get the report SQL statement content
+                // TEST...
                 $fileContent = file_get_contents($filePointer);
 
                 // Copy all the request variables into the Prepared Values,

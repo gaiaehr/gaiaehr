97ac16f64334c74bfd5801b9ecd5b85420d55cf4
Gino Rivera <hrivera@tranextgen.com>
2016-02-09 10:36:15 -0400

    PhiMail: - Fixed the module PhiMail Connector and Settings, set up an set_error_handler to handle PHP Warnings, Notice, Fatal

--- app/view/patient/Results.js
+++ app/view/patient/Results.js
@@ -148,9 +148,7 @@ Ext.define('App.view.patient.Results', {
                     {
                         var dataOrdered = record.data.date_ordered;
                         if(record.data.void)
-                        {
                             return '<span style="text-decoration: line-through;">'+dataOrdered+'</span>';
-                        }
                         return '<span>'+dataOrdered+'</span>';
                     }
                 },
@@ -163,9 +161,7 @@ Ext.define('App.view.patient.Results', {
                     renderer: function(v, meta, record)
                     {
                         if(record.data.void)
-                        {
                             return '<span style="text-decoration: line-through;">'+ v + '</span>';
-                        }
                         return '<span>'+ v + '</span>';
                     }
 				},
@@ -178,9 +174,7 @@ Ext.define('App.view.patient.Results', {
                     renderer: function(v, meta, record)
                     {
                         if(record.data.void)
-                        {
                             return '<span style="text-decoration: line-through;">'+ v + '</span>';
-                        }
                         return '<span>'+ v + '</span>';
                     }
 				}
